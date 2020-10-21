e9c74a831c256a150cae1328f8291e4dc01f2224
kazuki-nishio <71393106+kazuki-nishio@users.noreply.github.com>
2020-10-21 09:28:26 +0900

    Initial commit

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    //スクロール速度
+    private float scrollSpeed = -1;
+    //背景位置終了
+    private float deadLine = -16;
+    //背景位置開始位置
+    private float startLine = 15.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //背景を移動させる
+        transform.Translate(this.scrollSpeed * Time.deltaTime, 0, 0);
+        //画面外に出たら画面右端に移動する
+        if (transform.position.x < this.deadLine)
+        {
+            transform.position = new Vector2(this.startLine, 0);
+        }
+    }
+}
---
+++ Assets/CubeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    //キューブの移動速度
+    private float speed = -12;
+    //消滅位置
+    private float deadLine = -10;
+    AudioSource audio;
+    public AudioClip audioClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+        //画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //キューブが他のオブジェクトと接触したら音を鳴らす
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        audio.PlayOneShot(audioClip, 0.5f);
+        //ユニティちゃんと接触した場合は鳴らさない
+        if(collision.gameObject.tag=="UnityChanTag")
+        {
+          audio.volume = 0;
+        }
+    }
+}
